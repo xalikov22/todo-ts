f75ad05d901558ffbdf88f9113e0ada773f90292
Niels <niels.koster@portavita.nl>
2022-09-01 15:11:00 +0200

    Add styling to todo item

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,19 +2,23 @@ import './TodoItem.css'
 import React, {useContext} from 'react'
 import {TodoContext, TodoType} from '../context/TodoContext'
 import {Types} from '../context/reducers'
+import DeleteIcon from '../assets/delete-icon.svg'
 
-function TodoItem({id, title, task}: TodoType) {
+function TodoItem({id, title, task, color}: TodoType) {
 
   const {dispatch} = useContext(TodoContext)
 
   const onClick = () => {
-    dispatch({type: Types.Delete, payload: {id}})
+    if (confirm(`Delete todo item "${title}"?`)) {
+      dispatch({type: Types.Delete, payload: {id}})
+    }
   }
+
   return (
-    <div className={'TodoItem'}>
-      <h4>{title}</h4>
-      <div>{task}</div>
-      <button onClick={onClick}>delete</button>
+    <div className={'TodoItem'} style={{backgroundColor: `rgb(${color.red},${color.green},${color.blue}`}}>
+      <div className={'title'}>{title}</div>
+      <div className={'task'}>{task}</div>
+      <button className={'btnCircle btnBackgroundColor btnColor'} onClick={onClick}><img src={DeleteIcon} alt={'Delete'} title={'Delete'}/></button>
     </div>
   )
 }
