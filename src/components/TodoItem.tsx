6e9d5d46eb4d1c43b0bec28ee120666037467507
Niels <niels.koster@portavita.nl>
2022-09-01 12:03:24 +0200

    Item can be removed from list

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,11 +1,20 @@
-import {TodoItemType} from '../hooks/useTodo'
 import './TodoItem.css'
+import React, {useContext} from 'react'
+import {TodoContext, TodoType} from '../context/TodoContext'
+import {Types} from '../context/reducers'
 
-function TodoItem({id, title, task}: TodoItemType) {
+function TodoItem({id, title, task}: TodoType) {
+
+  const {dispatch} = useContext(TodoContext)
+
+  const onClick = () => {
+    dispatch({type: Types.Delete, payload: {id}})
+  }
   return (
     <div className={'TodoItem'}>
       <h4>{title}</h4>
-      <div>{id} {task}</div>
+      <div>{task}</div>
+      <button onClick={onClick}>delete</button>
     </div>
   )
 }
