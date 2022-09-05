ae98d0c04e83e81cc2d63882a0e05bc176df3122
Niels <niels.koster@portavita.nl>
2022-09-06 00:08:45 +0200

    Add drag&drop

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
-import {useContext, useEffect, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {TodoContext} from '../context/TodoContext'
-import {Types, TodoType} from '../context/reducers'
+import {TodoType, Types} from '../context/reducers'
 import TodoItem from './TodoItem'
 import {uuid} from '../util'
 
@@ -36,6 +36,31 @@ function TodoList() {
     setItemText('')
   }
 
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    e.dataTransfer.setData('text', e.currentTarget.id)
+  }
+
+  const enableDropping = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault()
+  }
+
+  const swap = ((arr:Array<any>, a:any, b:any) => {
+    const temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+    return arr
+  })
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault()
+    const targetId = Number(event.currentTarget.id.split('_')[1])
+    const sourceId = Number(event.dataTransfer.getData('text').split('_')[1])
+    todosDispatch({
+      type: Types.Relist,
+      payload: swap(todosState.todos, sourceId, targetId)
+    })
+  }
+
   return (
     <div>
       <div>
@@ -63,15 +88,26 @@ function TodoList() {
         </div>
       </div>
       <div>
-        {todosState.todos?.map((item: TodoType) =>
-          <div key={item.id}>
-            <TodoItem
-              id={item.id}
-              task={item.task}
-              title={item.title}
-              state={item.state}
-              color={item.color}
-            />
+        {todosState.todos?.map((item: TodoType, index) =>
+          <div
+            key={item.id}
+            id={`parent_${index}`}
+            onDragOver={enableDropping}
+            onDrop={handleDrop}
+          >
+            <div
+              draggable={true}
+              onDragStart={handleDragStart}
+              id={`child_${index}`}
+            >
+              <TodoItem
+                id={item.id}
+                task={item.task}
+                title={item.title}
+                state={item.state}
+                color={item.color}
+              />
+            </div>
           </div>)}
       </div>
     </div>
