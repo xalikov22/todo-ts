8872491cc3574febbb23904a708ca187870c0253
Niels <n.koster@portavita.eu>
2022-09-23 01:01:11 +0200

    More human-like stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -13,12 +13,27 @@ function Stats() {
 
   return (
     <div className={`Stats`}>
-      {todosState.todos.length} task{todosState.todos.length == 1 ? '' : 's'}, &nbsp;
-      {todosState.todos.filter((item) => item.state == 'doing').length} doing, &nbsp;
-      {todo} to do, &nbsp;
-      {todosState.todos.filter((item) => item.state == 'finished').length} done
+      {speak(todosState.todos.length)} task{todosState.todos.length == 1 ? '' : 's'} •
+      doing {speak(todosState.todos.filter((item) => item.state == 'doing').length)} •&nbsp;
+      {speak(todo)} to do •&nbsp;
+      {speak(todosState.todos.filter((item) => item.state == 'finished').length)} done
     </div>
   )
 }
 
 export default Stats
+
+function speak(n: number): string {
+  if (n === 0) return 'nothing'
+  if (n === 1) return 'one'
+  if (n === 2) return 'two'
+  if (n === 3) return 'three'
+  if (n === 4) return 'four'
+  if (n === 5) return 'five'
+  if (n === 6) return 'six'
+  if (n === 7) return 'seven'
+  if (n === 8) return 'eight'
+  if (n === 9) return 'nine'
+  if (n === 10) return 'ten'
+  return `${n}`
+}
